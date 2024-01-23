7301445f2674e6a6655ca7a2e8b9e44515e163cc
caolong201 <126745101+caolong201@users.noreply.github.com>
2024-01-23 11:32:03 +0700

    CommitAll

---
+++ Assets/Script/Dialogs/ShopPlayerDialog.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+public class ShopPlayerDialog : MonoBehaviour {
+	public  Text txt;
+	public UIGrid content;
+	public GameObject itemsPref;
+	public Text money;
+	public PanelRePlayDialog panelreplay;
+	//=====
+	public Transform playerState;
+	
+	public void Init()
+	{
+
+		money.text = AvPlayerManager.Instance.Money.ToString();
+		AvGameObjectUtils.ClearAllChild (content.gameObject);
+		for (int i = 0; i <  AvPlayerManager.Instance.PlayerCount; i++)
+		{
+			GameObject item = Instantiate(itemsPref) as GameObject;
+			item.transform.SetParent(content.gameObject.transform);
+			item.transform.localScale =Vector3.one;
+			ConfigShopPlayerItems configItem =AvPlayerManager.Instance.ListConfigShopPlayerItems[i];
+			item.GetComponent<ItemShopPlayer>().Init(configItem.id,configItem.name,configItem.money);
+		}
+		content.Reposition ();
+	}
+	public void OnbtnBACKclicked()
+	{
+		SoundManager.Instance.PlayButtonClick ();
+		//save current player id
+		SaveDataManager.Instance.SetCurrentPlayerID ();
+		Destroy (AvGameObjectUtils.FindGameObjectWithTagPlayer ());
+		GameObject go =	AvGameObjectUtils.LoadGameObject (playerState, GameConstance.PathOfPlayerPrefabs+AvPlayerManager.Instance.CurrentPlayerID);
+		GuiManager.Instance.HideDialog (gameObject);
+
+		panelreplay.CheckShowIconNew ();
+		//close dialog shop
+	}
+}
---
+++ Assets/Script/Editor/EditorCommon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+public class EditorCommon : MonoBehaviour {
+
+	[MenuItem ("Tuong/Clear All Prefs")]
+	static void ClearPrefs () {
+		PlayerPrefs.DeleteAll ();
+		Debug.Log ("Clear all Prefabs success");
+	}
+}
